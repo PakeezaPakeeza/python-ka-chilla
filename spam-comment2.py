d5992359116eb3ff0cb08a42adbe038c121081a3
pakeeza aka cloud engineer <zunairapakeeza3@gmail.com>
2024-06-23 15:03:30 +0200

    ker do commit

--- spam-comment2.py
+++ spam-comment2.py
@@ -1,6 +1,6 @@
 p1="make a lot of money"
 p2="buy now"
-p3="subscribe this"
+p3="subscribe this channel"
 p4="click this"
 message=input("enter your comment  ")
 if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
